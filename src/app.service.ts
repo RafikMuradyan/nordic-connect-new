55c4c345fced5dd3d4140796582663c008b6cf82
RafikMuradyan <muradyanrafik1@gmail.com>
2024-06-14 23:31:47 +0400

    added env

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const p = process.env.PORT;
+    return `Hello World! 4{port}`;
   }
 }
