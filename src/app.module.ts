e4a8577558846c28372ac69aa9593295fa9b6fbc
RafikMuradyan <muradyanrafik1@gmail.com>
2024-06-14 23:38:36 +0400

    connected to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 require("dotenv").config();
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URI, {
+      dbName: "nordic-connect",
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
